015410ad701056f626267578edffca6136c42a40
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-06-26 10:06:14 +0100

    sync

--- ml/options/pix2pix.py
+++ ml/options/pix2pix.py
@@ -69,7 +69,7 @@ class Pix2pixTrainOptions(Pix2pixOptions, BaseTrainOptions):
     def save_freq(self): return 1
 
     @property
-    def batch_log_freq(self): return 2
+    def batch_log_freq(self): return 10
 
     @property
     def num_workers(self):

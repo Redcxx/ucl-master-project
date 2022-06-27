e5341ef240ee3e8228b750d94074b794807dc2b4
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-06-27 23:03:11 +0100

    sync

--- inference.py
+++ inference.py
@@ -6,8 +6,8 @@ from ml.options import create_inference_options
 def main():
     name = 'alac_gan'
     opt = create_inference_options(name)
-    train_loader, test_loader = create_inference_dataloaders(opt, name)
-    model = create_inference_model(opt, train_loader, test_loader)
+    inference_loader = create_inference_dataloaders(opt, name)
+    model = create_inference_model(opt, inference_loader)
 
     print('Inference started')
     model.inference()

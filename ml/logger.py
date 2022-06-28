af2f548c5b48d11f1d4e624478170cd7b0043118
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-06-28 20:33:25 +0100

    sync

--- ml/logger.py
+++ ml/logger.py
@@ -3,7 +3,7 @@ import os
 LOG_FILE = 'log.txt'
 
 
-def log(text, save_local=True, end=os.pathsep):
+def log(text, save_local=True, end=os.linesep):
     text = str(text) + str(end)
     if save_local:
         with open(LOG_FILE, 'a') as file:

2c215fcc1b1ee0cfea8b1afb6cc0dc3d54b48e64
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-08-31 00:08:49 +0100

    sync

--- ml/options/alac_gan.py
+++ ml/options/alac_gan.py
@@ -7,13 +7,13 @@ class AlacGANInferenceOptions(BaseInferenceOptions):
         super().__init__()
         # self.input_images_path = r'./colorization/test'
         self.input_images_path = r'alacgan_colorization_data/test'
-        self.output_images_path = 'pretrain_colorization_output'
+        self.output_images_path = 'pretrain_colorization_output_lighter'
         self.image_size = 512
         self.a_to_b = True
-        self.batch_size = 1
-        self.num_workers = 1
+        self.batch_size = 4
+        self.num_workers = 4
         self.hint_mask = True
-        self.hint_multiplier = 1
+        self.hint_multiplier = 0.2
         self.custom_color = None  # (235, 64, 52)
         self.limit = 100
 

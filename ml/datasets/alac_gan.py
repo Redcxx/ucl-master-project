f0fe85a818150bdfb0eeb0bae6f7e1a076fdf40f
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-08-31 02:36:30 +0100

    sync

--- ml/datasets/alac_gan.py
+++ ml/datasets/alac_gan.py
@@ -240,9 +240,9 @@ class AlacGANInferenceDataset(BaseDataset):
         if self.custom_color is not None:
             gray = cv.cvtColor(B, cv.COLOR_RGB2GRAY)
             gray = gray.astype(float) / 255
-            gray = np.repeat(gray[np.newaxis, ...], 3, 0)  # add rgb channels
-            color = self.custom_color[..., np.newaxis, np.newaxis]  # add image size channels
-            im = (gray * color).astype(int)
+            gray = np.repeat(gray[..., np.newaxis], 3, 0)  # add rgb channels
+            color = self.custom_color[np.newaxis, np.newaxis, ...]  # add image size channels
+            im = (gray * color).astype(np.uint8)
             v_im = self.v_trans(self._cv2pil_im(im))
         else:
             v_im = self.v_trans(c_im)

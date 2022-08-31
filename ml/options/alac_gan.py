0a276f23aef08c5e4c8cd5841778990b5ca033c5
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-08-31 02:58:12 +0100

    sync

--- ml/options/alac_gan.py
+++ ml/options/alac_gan.py
@@ -7,14 +7,14 @@ class AlacGANInferenceOptions(BaseInferenceOptions):
         super().__init__()
         # self.input_images_path = r'./colorization/test'
         self.input_images_path = r'alacgan_colorization_data/test'
-        self.output_images_path = 'pretrain_colorization_output_red'
+        self.output_images_path = 'pretrain_colorization_output_blue'
         self.image_size = 512
         self.a_to_b = True
         self.batch_size = 8
         self.num_workers = 4
         self.hint_mask = True
         self.hint_multiplier = 1
-        self.custom_color = (235, 64, 52)  # red: (235, 64, 52), yellow: (252, 186, 3)
+        self.custom_color = (66, 135, 245)  # red: (235, 64, 52), yellow: (252, 186, 3), blue: (66, 135, 245)
         self.limit = 100
 
     @property

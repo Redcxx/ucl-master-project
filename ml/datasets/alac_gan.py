3719e4be68cb738ab1d86862eff32fa59da98050
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-09-01 16:28:43 +0100

    sync

--- ml/datasets/alac_gan.py
+++ ml/datasets/alac_gan.py
@@ -69,7 +69,9 @@ class AlacGANTrainDataset(BaseDataset):
         s_im = s_im.convert('L')
         if random.random() < 0.5:
             c_im, s_im = c_im.transpose(Image.FLIP_LEFT_RIGHT), s_im.transpose(Image.FLIP_LEFT_RIGHT)
-        c_im, v_im, s_im = self.c_trans(c_im), self.v_trans(c_im), self.s_trans(s_im)
+        c_im = self.c_trans(c_im)
+        v_im = self.v_trans(c_im)
+        s_im = self.s_trans(s_im)
 
         return c_im, v_im, s_im
 

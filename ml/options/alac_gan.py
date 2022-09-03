a8b13597c94074131265ef142a7ae92f1efa47db
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-09-03 10:34:21 +0100

    sync

--- ml/options/alac_gan.py
+++ ml/options/alac_gan.py
@@ -47,9 +47,9 @@ class AlacGANTrainOptions(BaseTrainOptions):
         self.batch_size = 8
         self.start_epoch = 1
         self.end_epoch = 300
-        self.eval_freq = 50
+        self.eval_freq = 20
         self.log_freq = 1
-        self.save_freq = 50
+        self.save_freq = 20
         self.batch_log_freq = 0
         self.resume_ckpt_file = 'alacGAN-train-2022-06-29-Wednesday-17h-23m-26s_final.ckpt'
 
@@ -62,10 +62,10 @@ class AlacGANTrainOptions(BaseTrainOptions):
         self.out_channels = 3
 
         # Optimizer
-        self.lr = 0.000001
+        self.lr = 0.00002
 
         # Scheduler
-        self.scheduler_step_size = 150
+        self.scheduler_step_size = 100
         self.scheduler_gamma = 0.5
 
         # Backbones checkpoint

040b5e3f3359e820840802891c69087a0ffd15be
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-09-04 16:45:36 +0100

    sync

--- ml/models/alac_gan.py
+++ ml/models/alac_gan.py
@@ -338,12 +338,10 @@ class AlacGANTrainModel(BaseTrainModel):
         log('Sanity Checks Generated')
 
     def pre_train(self):
-        log('pre train')
         super().pre_train()
         # self._sanity_check()
 
     def pre_epoch(self):
-        log('pre epoch')
         super().pre_epoch()
         self.net_G_losses = []
         self.net_D_losses = []
@@ -351,11 +349,9 @@ class AlacGANTrainModel(BaseTrainModel):
         self.l1_losses = []
 
     def pre_batch(self, epoch, batch):
-        log('pre batch')
         super().pre_batch(epoch, batch)
 
     def train_batch(self, batch, batch_data):
-        log(f'batch={batch}')
 
         self.net_G.train()
         self.net_D.train()

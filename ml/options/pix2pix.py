4a0b31b3791752f7ce7953c9804a1a5c000ffbab
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-09-09 13:42:00 +0100

    sync

--- ml/options/pix2pix.py
+++ ml/options/pix2pix.py
@@ -46,8 +46,8 @@ class Pix2pixTrainOptions(Pix2pixOptions, BaseTrainOptions):
 
         # Training
         self.batch_size = 8
-        # self.run_id = 'pix2pix-sketch-simplification-MSE-DILATE-CONTENT_LOSS-2022-09-08-Thursday-14h-26m-24s'
-        self.start_epoch = 1
+        self.run_id = 'pix2pix-sketch-simplification-NO_WEIGHT_MAP-MSE-2022-09-09-Friday-10h-01m-56s'
+        self.start_epoch = 301
         self.end_epoch = 500
         self.eval_freq = 50
         self.log_freq = 5
@@ -71,8 +71,8 @@ class Pix2pixTrainOptions(Pix2pixOptions, BaseTrainOptions):
 
         self.weight_map = False
         self.dilate = False
-        self.content_loss = True
-        self.mse_loss = False
+        self.content_loss = False
+        self.mse_loss = True
         # self.resume_ckpt_file = 'pix2pix-sketch-simplification-DILATE-2022-09-07-Wednesday-13h-35m-26s'
 
         # Model

7e31c04ca1ad59b2a9562eb168d9a210e46e9fa1
Redcxx <weilue.luo.21@ucl.ac.uk>
2022-09-09 10:16:39 +0100

    sync

--- ml/options/pix2pix.py
+++ ml/options/pix2pix.py
@@ -17,7 +17,7 @@ class Pix2pixInferenceOptions(BaseInferenceOptions):
     def __init__(self):
         super().__init__()
         self.input_images_path = r'sketch_simplification/test'
-        self.output_images_path = 'noghost_sketch_simplification_WEIGHT_MAP_DILATE_CONTENT_LOSS'
+        self.output_images_path = 'noghost_sketch_simplification_MSE_CONTENT_LOSS'
         self.image_size = 512
         self.a_to_b = True
         self.batch_size = 8
@@ -28,11 +28,11 @@ class Pix2pixInferenceOptions(BaseInferenceOptions):
 
     @property
     def tag(self):
-        return 'pix2pix-noghost-inference-sketch-simplification-WEIGHT_MAP-DILATE-CONTENT_LOSS'
+        return 'pix2pix-noghost-inference-sketch-simplification-NO-WEIGHT_MAP-MSE-CONTENT_LOSS'
 
     @property
     def inference_run_id(self):
-        return 'pix2pix-sketch-simplification-CONTENT_LOSS-DILATE-2022-09-07-Wednesday-23h-16m-58s'
+        return 'pix2pix-sketch-simplification-NO_WEIGHT_MAP-MSE-CONTENT_LOSS-2022-09-09-Friday-00h-15m-15s'
 
     @property
     def inference_run_tag(self):
@@ -55,14 +55,12 @@ class Pix2pixTrainOptions(Pix2pixOptions, BaseTrainOptions):
         self.batch_log_freq = 0
 
         # Dataset
-        # # pix2pix-sketch-simplification-NO_WEIGHT_MAP-MSE-2022-09-08-Thursday-10h-28m-04s
-        # # pix2pix-sketch-simplification-CONTENT_LOSS-DILATE-2022-09-07-Wednesday-23h-16m-58s + weightmap
-
         # pix2pix-sketch-simplification-NO_WEIGHT_MAP-MSE-CONTENT_LOSS-2022-09-09-Friday-00h-15m-15s
 
-        # pix2pix-sketch-simplification-DILATE-2022-09-07-Wednesday-13h-35m-26s + weightmap
-        # pix2pix-sketch-simplification-CONTENT_LOSS-2022-09-07-Wednesday-19h-44m-34s + weightmap
-        # pix2pix-sketch-simplification-NORMAL-2022-09-07-Wednesday-16h-09m-49s + weightmap
+        # pix2pix-sketch-simplification-CONTENT_LOSS-DILATE-2022-09-07-Wednesday-23h-16m-58s
+        # pix2pix-sketch-simplification-DILATE-2022-09-07-Wednesday-13h-35m-26s
+        # pix2pix-sketch-simplification-CONTENT_LOSS-2022-09-07-Wednesday-19h-44m-34s
+        # pix2pix-sketch-simplification-NORMAL-2022-09-07-Wednesday-16h-09m-49s
         # pix2pix-sketch-simplification-NO-WEIGHT-MAP-2022-09-08-Thursday-10h-16m-21s
         # pix2pix-sketch-simplification-NO_WEIGHT_MAP-CONTENT_LOSS-2022-09-08-Thursday-18h-11m-45s
 
